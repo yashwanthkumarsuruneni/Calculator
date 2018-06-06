2eefde71148e30951cb7d5d06a3d341a1485acb5
Yashwanth Suruneni <yashwanthsuruneni@gmail.com>
2018-06-06 10:35:13 -0400

    HMR working

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,45 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
-  },
-  devServer: {
-    historyApiFallback: true,
-    contentBase: './'
+  entry: {
+      app: './src/index.js'
   },
   devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist',
+    hot: true
+  },
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+      template: 'src/index.html'
+    }),
+     new webpack.NamedModulesPlugin(),
+     new webpack.HotModuleReplacementPlugin()
+  ],
   module: {
-         rules: [
-            {
-                exclude: '/node_modules/',
-                loader: 'babel-loader',
-                query: {
-                    presets: ['es2015']
-                }
-            },
-           {
-            test: /\.css$/,
-             use: [
-               'style-loader',
-               'css-loader'
-             ]
-           }
-         ]
-  }   
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+       test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      }
+    ]
+ },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  }
 };
